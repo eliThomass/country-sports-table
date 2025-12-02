c7bf08e5bed34af5d82bfdd0f6dca24f68b0f1fb
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-12-02 10:03:27 -0800

    added insert statements for countrysport and sport tables

---
+++ insert.sql
@@ -0,0 +1,118 @@
+-- Inserts given by professor below
+
+-- Data for Sport table
+
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (1, 'Aeronautics', 'Motorsports', 'Switzerland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (2, 'Alpine Skiing', 'Winter Sports', 'Austria');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (3, 'Alpinism', 'Winter Sports', 'Europe');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (4, 'Archery', 'Target Sports', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (5, 'Art Competitions', 'Art', 'Greece');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (6, 'Athletics', 'Athletics', 'Greece');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (7, 'Badminton', 'Racket Sports', 'British India');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (8, 'Baseball', 'Team Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (9, 'Basketball', 'Team Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (10, 'Basque Pelota', 'Racket Sports', 'Spain');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (11, 'Beach Volleyball', 'Team Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (12, 'Biathlon', 'Winter Sports', 'Scandinavia');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (13, 'Bobsleigh', 'Winter Sports', 'Switzerland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (14, 'Boxing', 'Combat Sports', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (15, 'Canoeing', 'Water Sports', 'Canada');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (16, 'Cricket', 'Team Sports', 'England');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (17, 'Croquet', 'Target Sports', 'France');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (18, 'Cross Country Skiing', 'Winter Sports', 'Norway');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (19, 'Curling', 'Winter Sports', 'Scotland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (20, 'Cycling', 'Cycling', 'Europe');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (21, 'Diving', 'Water Sports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (22, 'Equestrianism', 'Equestrian', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (23, 'Fencing', 'Combat Sports', 'Europe');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (24, 'Figure Skating', 'Winter Sports', 'Europe');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (25, 'Football', 'Team Sports', 'England');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (26, 'Freestyle Skiing', 'Winter Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (27, 'Golf', 'Target Sports', 'Scotland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (28, 'Gymnastics', 'Gymnastics', 'Greece');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (29, 'Handball', 'Team Sports', 'Germany');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (30, 'Hockey', 'Team Sports', 'England');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (31, 'Ice Hockey', 'Winter Sports', 'Canada');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (32, 'Jeu De Paume', 'Racket Sports', 'France');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (33, 'Judo', 'Combat Sports', 'Japan');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (34, 'Lacrosse', 'Team Sports', 'Canada');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (35, 'Luge', 'Winter Sports', 'Switzerland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (36, 'Military Ski Patrol', 'Winter Sports', 'Switzerland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (37, 'Modern Pentathlon', 'Athletics', 'Sweden');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (38, 'Motorboating', 'Motorsports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (39, 'Nordic Combined', 'Winter Sports', 'Norway');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (40, 'Polo', 'Equestrian', 'Persia');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (41, 'Racquets', 'Racket Sports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (42, 'Rhythmic Gymnastics', 'Gymnastics', 'Russia');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (43, 'Roque', 'Target Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (44, 'Rowing', 'Water Sports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (45, 'Rugby', 'Team Sports', 'England');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (46, 'Rugby Sevens', 'Team Sports', 'Scotland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (47, 'Sailing', 'Water Sports', 'Netherlands');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (48, 'Shooting', 'Target Sports', 'Europe');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (49, 'Short Track Speed Skating', 'Winter Sports', 'USA/Canada');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (50, 'Skeleton', 'Winter Sports', 'Switzerland');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (51, 'Ski Jumping', 'Winter Sports', 'Norway');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (52, 'Snowboarding', 'Winter Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (53, 'Softball', 'Team Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (54, 'Speed Skating', 'Winter Sports', 'Netherlands');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (55, 'Swimming', 'Water Sports', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (56, 'Synchronized Swimming', 'Water Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (57, 'Table Tennis', 'Racket Sports', 'England');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (58, 'Taekwondo', 'Combat Sports', 'Korea');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (59, 'Tennis', 'Racket Sports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (60, 'Trampolining', 'Gymnastics', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (61, 'Triathlon', 'Athletics', 'France/USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (62, 'Tug-Of-War', 'Team Sports', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (63, 'Volleyball', 'Team Sports', 'USA');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (64, 'Water Polo', 'Water Sports', 'UK');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (65, 'Weightlifting', 'Strength Sports', 'Ancient');
+INSERT INTO Sport (SportID, SportName, Type, CountryOrigin) VALUES (66, 'Wrestling', 'Combat Sports', 'Ancient');
+
+-- Data for CountrySport table
+
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('USA', 8, 15.5, 2); -- USA, Baseball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('USA', 9, 22.1, 3); -- USA, Basketball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('USA', 25, 18.0, 5); -- USA, Football (Soccer)
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('BRA', 25, 65.0, 1); -- Brazil, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('BRA', 63, 20.5, 2); -- Brazil, Volleyball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CAN', 31, 35.0, 1); -- Canada, Ice Hockey
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CAN', 19, 12.5, 3); -- Canada, Curling
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('IND', 16, 45.0, 1); -- India, Cricket
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('IND', 30, 8.5, 5); -- India, Field Hockey
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CHN', 57, 30.0, 1); -- China, Table Tennis
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CHN', 7, 25.0, 2); -- China, Badminton
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('GBR', 25, 32.0, 1); -- UK, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('GBR', 16, 15.0, 3); -- UK, Cricket
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('AUS', 16, 20.0, 1); -- Australia, Cricket
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('AUS', 55, 28.5, 2); -- Australia, Swimming
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('AUS', 45, 18.0, 3); -- Australia, Rugby
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('JPN', 8, 25.0, 1); -- Japan, Baseball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('JPN', 33, 10.0, 4); -- Japan, Judo
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('FRA', 20, 14.5, 3); -- France, Cycling
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('FRA', 25, 40.0, 1); -- France, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('DEU', 25, 45.0, 1); -- Germany, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('DEU', 29, 12.0, 5); -- Germany, Handball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('RUS', 31, 28.0, 1); -- Russia, Ice Hockey
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('RUS', 24, 9.5, 4); -- Russia, Figure Skating
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('NOR', 18, 30.0, 1); -- Norway, Cross Country Skiing
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('NZL', 45, 22.0, 1); -- New Zealand, Rugby
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('KOR', 58, 15.0, 3); -- South Korea, Taekwondo
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('KOR', 4, 8.0, 5); -- South Korea, Archery
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ARG', 25, 60.0, 1); -- Argentina, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CUB', 8, 35.0, 1); -- Cuba, Baseball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ITA', 25, 55.0, 1); -- Italy, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ITA', 20, 12.0, 4); -- Italy, Cycling
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('NLD', 54, 18.0, 2); -- Netherlands, Speed Skating
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('NLD', 25, 42.0, 1); -- Netherlands, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('JAM', 6, 20.0, 1); -- Jamaica, Athletics
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('PHL', 9, 40.0, 1); -- Philippines, Basketball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('AUT', 2, 35.0, 1); -- Austria, Alpine Skiing
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('FIN', 31, 28.0, 1); -- Finland, Ice Hockey
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ESP', 25, 50.0, 1); -- Spain, Football
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ESP', 59, 15.0, 3); -- Spain, Tennis
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('CHE', 2, 25.0, 1); -- Switzerland, Alpine Skiing
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ZAF', 45, 12.0, 2); -- South Africa, Rugby
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ZAF', 16, 10.0, 3); -- South Africa, Cricket
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('ABW', 8, 14.0, 1); -- Aruba, Baseball
+INSERT INTO CountrySport (CountryCode, SportID, ParticipationRate, Popularity) VALUES ('PAK', 16, 50.0, 1); -- Pakistan, Cricket
