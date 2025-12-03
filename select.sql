fb5573a1e5197b3a7f595b271447addc485589f6
Brianna Zaffina <83510513+zaffinabrianna@users.noreply.github.com>
2025-12-02 21:37:39 -0800

    implemented the remaining 5 subqueries.

--- select.sql
+++ select.sql
@@ -24,3 +24,77 @@ SELECT * FROM Sport;
 SELECT c.Name, s.SportID FROM Country AS c
 RIGHT JOIN CountrySport as s
 ON c.Code = s.CountryCode;
+
+-- Brianna's Queries
+
+-- Subquery of sports that are more popular than the average popularity. 
+SELECT SportName -- Select the SportName variable from the "Sport" table
+FROM Sport 
+WHERE SportID IN ( -- Find the SportID in table "CountrySport" that meet the following conditions
+	SELECT SportID 
+    FROM CountrySport 
+    WHERE Popularity > ( -- Specifically, the SportID where the popularity is greater than the average popularity
+		SELECT AVG(Popularity) -- Calc avg popularity in CountrySport table
+        FROM CountrySport 
+        )
+	);
+    
+-- Subquery of the sports that are played by the country with the highest population
+SELECT SportName
+FROM Sport
+WHERE SportID IN (
+	SELECT SportID
+    FROM CountrySport
+    WHERE CountryCode = (
+		SELECT Code
+        FROM Country
+        ORDER BY Population DESC
+        LIMIT 1
+	)
+);
+
+-- Subquery that shows the LEAST popular sport in countries where Italian is spoken
+SELECT SportName
+From Sport
+Where SportID = (
+	SELECT SportID
+    FROM CountrySport
+    WHERE CountryCode IN (
+		SELECT CountryCode
+        FROM CountryLanguage 
+        WHERE Language = 'Italian' AND IsOfficial = 'T'
+	)
+    ORDER BY Popularity ASC
+    LIMIT 1
+);
+    
+-- Subquery that finds the city in Italy with the lowest population
+SELECT Name as CityName, Population
+FROM City
+WHERE CountryCode = (
+	SELECT Code
+    FROM Country
+    WHERE Name = "Italy"
+)
+AND Population = (
+	SELECT MIN(Population)
+    FROM CITY
+    WHERE CountryCode = (
+		SELECT Code
+        FROM Country
+        WHERE Name = "Italy"
+	)
+);
+
+-- Subquery that finds the official language or languages of the country whos most popular sport is tennis
+SELECT CountryLanguage.Language
+FROM CountryLanguage 
+WHERE CountryLanguage.CountryCode = (
+	SELECT CountrySport.CountryCode
+    FROM CountrySport
+    JOIN Sport ON CountrySport.SportID = Sport.SportID
+    WHERE Sport.SportName = 'Tennis'
+    ORDER BY CountrySport.Popularity DESC
+    LIMIT 1
+)
+AND CountryLanguage.IsOfficial = 'T'
